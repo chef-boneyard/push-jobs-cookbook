33a4daadd4e659f77f249e48acb7f43ecd0400f2
John Keiser <jkeiser@opscode.com>
2013-10-28 13:43:44 -0700

    Bump version to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@opscode.com'
 license          'Apache 2.0'
 description      'Installs the Opscode Push Jobs Client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.0'
+version          '1.0.0'
 
 # Tested on Ubuntu 10.04, 12.04
 # Tested on CentOS 6.4

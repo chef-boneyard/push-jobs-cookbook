c0ecf37a3f716ef4105d72f56a3f5dc019df261b
Tim Smith <tsmith84@gmail.com>
2015-09-09 19:36:33 -0700

    add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'push-jobs'
 maintainer 'Chef Software, Inc.'
-maintainer_email 'cookbooks@getchef.com'
+maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
 description 'Installs the Chef Push Jobs Client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
@@ -17,3 +17,6 @@ supports 'windows'
 depends 'runit'
 depends 'windows'
 depends 'chef-ingredient'
+
+source_url 'https://github.com/chef-cookbooks/push-jobs' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/push-jobs/issues' if respond_to?(:issues_url)

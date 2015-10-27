835dca778c88d7312bf752cb89bd686573d1112d
John Keiser <john@johnkeiser.com>
2015-10-27 08:36:37 -0700

    Use a lazy value for values_to_set

--- recipes/service_windows.rb
+++ recipes/service_windows.rb
@@ -22,10 +22,7 @@ version = PushJobsHelper.parse_version(node, node['push_jobs']['package_url'])
 service_name = PushJobsHelper.windows_service_name(node, version)
 
 config_file_option = "-c #{PushJobsHelper.config_path}"
-
-values_to_set = [{ name: 'Parameters',
-                   type: :string,
-                   data: config_file_option }]
+key_path = "HKLM\\SYSTEM\\CurrentControlSet\\Services\\#{service_name}"
 
 # The Parameters key isn't respected by some versions. Inject the
 # config file path into ImagePath.
@@ -40,17 +37,24 @@ values_to_set = [{ name: 'Parameters',
 # The approach below parses the ImagePath from the installer, and adds
 # a command line option to point to the config file.
 #
-key_path = "HKLM\\SYSTEM\\CurrentControlSet\\Services\\#{service_name}"
-if registry_key_exists?(key_path)
-  values = registry_get_values(key_path)
-  imagepath = values.find { |x| x[:name] == 'ImagePath' }
-  match = imagepath[:data].match(/^(.*ruby\.exe)\s+(\S*windows_service\.rb)/)
-  imagepath[:data] = "#{match[1]} #{match[2]} #{config_file_option}"
-  values_to_set << imagepath
+
+def values_to_set
+  values_to_set = [{ name: 'Parameters',
+                     type: :string,
+                     data: config_file_option }]
+
+  if registry_key_exists?(key_path)
+    values = registry_get_values(key_path)
+    imagepath = values.find { |x| x[:name] == 'ImagePath' }
+    match = imagepath[:data].match(/^(.*ruby\.exe)\s+(\S*windows_service\.rb)/)
+    imagepath[:data] = "#{match[1]} #{match[2]} #{config_file_option}"
+    values_to_set << imagepath
+  end
+  values_to_set
 end
 
-registry_key "HKLM\\SYSTEM\\CurrentControlSet\\Services\\#{service_name}" do
-  values(values_to_set)
+registry_key key_path do
+  values lazy { values_to_set }
   notifies :restart, "service[#{service_name}]"
 end
 

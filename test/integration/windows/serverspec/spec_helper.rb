c1dd1215448c44668b39b18a953d1b05fb1015b2
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-11-04 15:44:26 -0500

    Skeleton windows serverspec test

---
+++ test/integration/windows/serverspec/spec_helper.rb
@@ -0,0 +1,5 @@
+require 'serverspec'
+require 'pathname'
+
+set :backend, :cmd
+set :os, family: 'windows'
---
+++ test/integration/windows/serverspec/windows_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'push-jobs::default recipe' do
+#  it 'Installs the push jobs client package' do
+#    expect(package 'opscode-push-jobs-client').to be_installed
+#  end
+#
+#  it 'Creates the push-jobs-client.rb' do
+#    expect(file '/etc/chef/push-jobs-client.rb').to be_file
+#    expect(file '/etc/chef/push-jobs-client.rb').to be_owned_by('root')
+#    expect(file '/etc/chef/push-jobs-client.rb').to be_grouped_into('root')
+#    expect(file '/etc/chef/push-jobs-client.rb').to contain('whitelist({"chef-client"=>"chef-client"})')
+#    expect(file '/etc/chef/push-jobs-client.rb').to contain("LC_ALL='en_US.UTF-8'")
+#  end
+end

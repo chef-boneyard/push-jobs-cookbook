35e2ea0948ca72b7450bee8ada960ab46f2ea628
David McCown <dmccown@chef.io>
2015-11-04 11:50:38 -0600

    Fix service_windows recipe to run registry fixup at runtime
    Currently the value to be written into the registry entry for the
    windows pushy client service is computed at compile time.  The registry
    value doesn't exist on a fresh machine before the first converge.
    Hence, the entry is never updated until a second converge.

--- recipes/service_windows.rb
+++ recipes/service_windows.rb
@@ -18,40 +18,38 @@
 # limitations under the License.
 #
 
+# For now, this is guaranteed to exist. Change this when
+# chef-ingredient supports installation on windows and we don't
+# force users to specify package_url.
 version = PushJobsHelper.parse_version(node, node['push_jobs']['package_url'])
 service_name = PushJobsHelper.windows_service_name(node, version)
 
 config_file_option = "-c #{PushJobsHelper.config_path}"
 
-values_to_set = [{ name: 'Parameters',
-                   type: :string,
-                   data: config_file_option }]
-
 # The Parameters key isn't respected by some versions. Inject the
 # config file path into ImagePath.
 #
-# This is a bit painful, since ImagePath contains a version specific
-# path to the windows_service.rb code; we can't know what it is until
-# the msi is installed. (The path incorporates the push-client ruby
-# gem version, which isn't always correlated with the version of the
-# omnibus package. So we can't create this entry before installation,
-# yet installation may fail if this isn't set.
+# The ImagePath looks like "X:\Path\To\ruby.exe X:\Path\To\windows_service.rb"
+# The MSI does not pass any other arguments. So we match the above
+# path, just to be extra careful, and then append the "-c path\to\config"
+# to it. We need to restart the windows service after performing this
+# to ensure that the change takes effect.
 #
-# The approach below parses the ImagePath from the installer, and adds
-# a command line option to point to the config file.
-#
+# If this registry key doesn't exist, then the service has not been
+# registered yet and an appropriate error will be thrown when
+# a service restart is attempted.
 key_path = "HKLM\\SYSTEM\\CurrentControlSet\\Services\\#{service_name}"
-if registry_key_exists?(key_path)
-  values = registry_get_values(key_path)
-  imagepath = values.find { |x| x[:name] == 'ImagePath' }
-  match = imagepath[:data].match(/^(.*ruby\.exe)\s+(\S*windows_service\.rb)/)
-  imagepath[:data] = "#{match[1]} #{match[2]} #{config_file_option}"
-  values_to_set << imagepath
-end
 
-registry_key "HKLM\\SYSTEM\\CurrentControlSet\\Services\\#{service_name}" do
-  values(values_to_set)
+registry_key key_path do
+  values lazy do
+    values = registry_get_values(key_path)
+    imagepath = values.find { |x| x[:name] == 'ImagePath' }
+    match = imagepath[:data].match(/^(.*ruby\.exe)\s+(\S*windows_service\.rb)/)
+    imagepath[:data] = "#{match[1]} #{match[2]} #{config_file_option}"
+    [imagepath]
+  end
   notifies :restart, "service[#{service_name}]"
+  only_if { registry_key_exists?(key_path) }
 end
 
 service service_name do

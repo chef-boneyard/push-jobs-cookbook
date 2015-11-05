f566bbd330e058afc00bfa93fe2810c86c2eda0f
Kartik Null Cating-Subramanian <ksubramanian@chef.io>
2015-11-05 15:00:12 -0500

    Serverspec tests for windows

--- test/integration/windows/serverspec/windows_spec.rb
+++ test/integration/windows/serverspec/windows_spec.rb
@@ -1,15 +1,18 @@
 require 'spec_helper'
 
 describe 'push-jobs::default recipe' do
-#  it 'Installs the push jobs client package' do
-#    expect(package 'opscode-push-jobs-client').to be_installed
-#  end
-#
-#  it 'Creates the push-jobs-client.rb' do
-#    expect(file '/etc/chef/push-jobs-client.rb').to be_file
-#    expect(file '/etc/chef/push-jobs-client.rb').to be_owned_by('root')
-#    expect(file '/etc/chef/push-jobs-client.rb').to be_grouped_into('root')
-#    expect(file '/etc/chef/push-jobs-client.rb').to contain('whitelist({"chef-client"=>"chef-client"})')
-#    expect(file '/etc/chef/push-jobs-client.rb').to contain("LC_ALL='en_US.UTF-8'")
-#  end
+  it 'Installs the push jobs client' do
+    expect(file 'C:/opscode/pushy/bin/pushy-client.bat').to be_file
+  end
+
+  it 'Enables the pushy client service' do
+    expect(service 'pushy-client').to be_installed
+    expect(service 'pushy-client').to be_enabled
+  end
+
+  it 'Creates the push-jobs-client.rb' do
+    expect(file 'C:/chef/push-jobs-client.rb').to be_file
+    expect((file 'C:/chef/push-jobs-client.rb').content).to include('whitelist({"chef-client"=>"chef-client"})')
+    expect((file 'C:/chef/push-jobs-client.rb').content).to include("LC_ALL='en_US.UTF-8'")
+  end
 end

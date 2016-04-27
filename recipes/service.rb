0dc52c4afe7ff6ed221ed07bcabec98798f28716
Tim Smith <tsmith84@gmail.com>
2016-04-27 14:48:57 -0700

    Update all copyrights

--- recipes/service.rb
+++ recipes/service.rb
@@ -6,7 +6,7 @@
 # Author:: Charles Johnson <charles@chef.io>
 # Author:: Christopher Maier <cm@chef.io>
 # Author:: Tyler Fitch <tfitch@chef.io>
-# Copyright 2013-2014 Chef Software, Inc. <legal@chef.io>
+# Copyright 2013-2016 Chef Software, Inc. <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.

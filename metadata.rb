5cc7f29f50953b2bb10b6f6221e04b4654b6d44d
Tim Smith <tsmith@chef.io>
2016-08-30 12:37:35 -0700

    Add chef_version to the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -19,5 +19,7 @@ depends 'windows', '>= 1.42.0'
 depends 'chef-ingredient', '>= 0.18.0'
 depends 'compat_resource'
 
-source_url 'https://github.com/chef-cookbooks/push-jobs' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/push-jobs/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/push-jobs'
+issues_url 'https://github.com/chef-cookbooks/push-jobs/issues'
+
+chef_version '>= 12.0' if respond_to?(:chef_version)

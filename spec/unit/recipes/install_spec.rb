62b023b52a2c1b786d39e99a57e85571462b00d9
Jerry Aldrich III <jerry@chef.io>
2016-12-10 21:52:06 -0500

    Add support for using a local Push Jobs package
    Signed-off-by: Jerry Aldrich III <jerry@chef.io>

--- spec/unit/recipes/install_spec.rb
+++ spec/unit/recipes/install_spec.rb
@@ -28,6 +28,29 @@ describe 'push-jobs::install' do
     end
   end
 
+  context 'with local_package_path' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04')
+      runner.node.normal['push_jobs']['local_package_path'] = '/tmp/opscode-push-jobs-client_1.0.0_amd64.deb'
+      runner.converge('recipe[push-jobs::install]')
+    end
+
+    before { @package_file = 'opscode-push-jobs-client_1.0.0_amd64.deb' }
+
+    it 'installs the chef_ingredient push-client' do
+      expect(chef_run).to install_chef_ingredient('push-jobs-client').with(package_source: '/tmp/opscode-push-jobs-client_1.0.0_amd64.deb')
+    end
+
+    it 'includes the config recipe' do
+      expect(chef_run).to include_recipe("#{described_cookbook}::config")
+    end
+
+    it 'starts the chef-push-jobs-client' do
+      expect(chef_run).to start_push_jobs_service_upstart 'push-jobs'
+      expect(chef_run).to enable_push_jobs_service_upstart 'push-jobs'
+    end
+  end
+
   context 'without package_url' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04')

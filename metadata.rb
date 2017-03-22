20594a6df087ce336372614a5aa8dc5ebe108f10
Tim Smith <tsmith@chef.io>
2017-03-21 21:05:13 -0700

    Update Apache license string in metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'push-jobs'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs the Chef Push Jobs Client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.0.0'
@@ -20,4 +20,4 @@ depends 'chef-ingredient', '>= 0.19.0'
 
 source_url 'https://github.com/chef-cookbooks/push-jobs'
 issues_url 'https://github.com/chef-cookbooks/push-jobs/issues'
-chef_version '>= 12.5'
+chef_version '>= 12.5' if respond_to?(:chef_version)

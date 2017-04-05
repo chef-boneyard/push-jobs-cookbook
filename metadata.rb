3c15561bf401b82efcf07d91266afdd611d154df
Jennifer Davis <iennae@gmail.com>
2017-04-05 13:10:30 -0700

    Add support for centos 6 using upstart.
    Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs the Chef Push Jobs Client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.0.2'
 
-# Tested on Ubuntu 12.04 - 16.04
+# Tested on Ubuntu 14.04 - 16.04
 # Tested on CentOS 6-7
 # Tested on Debian 7-8
 supports 'ubuntu'
